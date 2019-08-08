34d30ca8a4f6e0efb343e335e4c007f25a3db4d7
ramiz-rahman <ramiz.rahman@outlook.com>
2019-08-09 02:57:35 +0600

    Implement voting functionality

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
 import { AUTH_TOKEN } from '../constants';
 import { timeDifferenceForDate } from '../utils';
+import { Mutation } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
 
 class Link extends Component {
   render() {
@@ -10,9 +31,16 @@ class Link extends Component {
         <div className="flex items-center">
           <span className="gray">{this.props.index + 1}.</span>
           {authToken && (
-            <div className="ml1 gray fill" onClick={() => this._voteForLink()}>
-              ▲
-            </div>
+            <Mutation
+              mutation={VOTE_MUTATION}
+              variables={{ linkId: this.props.link.id }}
+            >
+              {voteMutation => (
+                <div className="ml1 gray fill" onClick={voteMutation}>
+                  ▲
+                </div>
+              )}
+            </Mutation>
           )}
         </div>
         <div className="ml1">
