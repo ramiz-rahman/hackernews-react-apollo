5024c6f156839f8646751baf7a0c2e896a7dd2aa
ramiz-rahman <ramiz.rahman@outlook.com>
2019-08-09 17:50:16 +0600

    Set up Apollo to handle subscriptions

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
+import { split } from 'apollo-link';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
 
 import { BrowserRouter } from 'react-router-dom';
 
@@ -29,9 +32,28 @@ const authLink = setContext((_, { headers }) => {
   };
 });
 
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000`;
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN);
+    }
+  }
+})
+
+const link = split(
+  ({ query }) => {
+    const { kind, operation } = getMainDefinition(query);
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink, 
+  authLink.concat(httpLink)
+)
+
 // 3
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
+  link,
   cache: new InMemoryCache()
 });
 
